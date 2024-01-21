175e4a9d24721f0b8da4480764c421b20740c505
ASoulLab <108452454+ASoulLab@users.noreply.github.com>
2024-01-21 09:48:48 +0000

    Remove extra file

--- constraint-programming/src/main/java/org/optaplanner/examples/cloudbalancing/app/CloudBalancingHelloWorld.kt
+++ constraint-programming/src/main/java/org/optaplanner/examples/cloudbalancing/app/CloudBalancingHelloWorld.kt
@@ -16,3 +16,39 @@
  */
 
 package org.optaplanner.examples.cloudbalancing.app
+
+import org.optaplanner.core.api.solver.Solver
+import org.optaplanner.core.api.solver.SolverFactory
+import org.optaplanner.examples.cloudbalancing.domain.CloudBalance
+import org.optaplanner.examples.cloudbalancing.domain.CloudComputer
+import org.optaplanner.examples.cloudbalancing.domain.CloudProcess
+import org.optaplanner.examples.cloudbalancing.persistence.CloudBalancingGenerator
+
+object CloudBalancingHelloWorld {
+
+    @JvmStatic fun main(args: Array<String>) {
+        // Build the Solver
+        val solverFactory = SolverFactory.createFromXmlResource<CloudBalance>(
+                "org/optaplanner/examples/cloudbalancing/solver/cloudBalancingSolverConfig.xml")
+        val solver = solverFactory.buildSolver()
+
+        // Load a problem with 400 computers and 1200 processes
+        val unsolvedCloudBalance = CloudBalancingGenerator().createCloudBalance(400, 1200)
+
+        // Solve the problem
+        val solvedCloudBalance = solver.solve(unsolvedCloudBalance)
+
+        // Display the result
+        println("\nSolved cloudBalance with 400 computers and 1200 processes:\n" + toDisplayString(solvedCloudBalance))
+    }
+
+    fun toDisplayString(cloudBalance: CloudBalance): String {
+        val displayString = StringBuilder()
+        for (process in cloudBalance.processList!!) {
+            val computer = process.computer
+            displayString.append("  ").append(process.label).append(" -> ").append(computer?.label).append("\n")
+        }
+        return displayString.toString()
+    }
+
+}
