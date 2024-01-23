2f84229606829f9e8b4e96558be538dddccc8b0d
ASoulLab <108452454+ASoulLab@users.noreply.github.com>
2024-01-23 17:04:06 +0000

    add logos

--- constraint-programming/src/main/java/org/optaplanner/examples/cloudbalancing/persistence/CloudBalancingGenerator.kt
+++ constraint-programming/src/main/java/org/optaplanner/examples/cloudbalancing/persistence/CloudBalancingGenerator.kt
@@ -120,4 +120,78 @@ class CloudBalancingGenerator : LoggingMain {
         for (i in 0..computerListSize - 1) {
             val computer = CloudComputer()
             computer.id = i.toLong()
-         
+            val cpuPowerPricesIndex = random.nextInt(CPU_POWER_PRICES.size)
+            computer.cpuPower = CPU_POWER_PRICES[cpuPowerPricesIndex].hardwareValue
+            val memoryPricesIndex = distortIndex(cpuPowerPricesIndex, MEMORY_PRICES.size)
+            computer.memory = MEMORY_PRICES[memoryPricesIndex].hardwareValue
+            val networkBandwidthPricesIndex = distortIndex(cpuPowerPricesIndex, NETWORK_BANDWIDTH_PRICES.size)
+            computer.networkBandwidth = NETWORK_BANDWIDTH_PRICES[networkBandwidthPricesIndex].hardwareValue
+            val cost = CPU_POWER_PRICES[cpuPowerPricesIndex].cost
+            +MEMORY_PRICES[memoryPricesIndex].cost
+            +NETWORK_BANDWIDTH_PRICES[networkBandwidthPricesIndex].cost
+            computer.cost = cost
+            logger.trace("Created computer with cpuPowerPricesIndex ({}), memoryPricesIndex ({})," + " networkBandwidthPricesIndex ({}).",
+                    cpuPowerPricesIndex, memoryPricesIndex, networkBandwidthPricesIndex)
+            computerList.add(computer)
+        }
+        cloudBalance.computerList = computerList
+    }
+
+    private fun distortIndex(referenceIndex: Int, length: Int): Int {
+        var index = referenceIndex
+        val randomDouble = random.nextDouble()
+        var loweringThreshold = 0.25
+        while (randomDouble < loweringThreshold && index >= 1) {
+            index--
+            loweringThreshold *= 0.10
+        }
+        var heighteningThreshold = 0.75
+        while (randomDouble >= heighteningThreshold && index <= length - 2) {
+            index++
+            heighteningThreshold = 1.0 - (1.0 - heighteningThreshold) * 0.10
+        }
+        return index
+    }
+
+    private fun createProcessList(cloudBalance: CloudBalance, processListSize: Int) {
+        val processList = ArrayList<CloudProcess>(processListSize)
+        for (i in 0..processListSize - 1) {
+            val process = CloudProcess()
+            process.id = i.toLong()
+            val requiredCpuPower = generateRandom(MAXIMUM_REQUIRED_CPU_POWER)
+            process.requiredCpuPower = requiredCpuPower
+            val requiredMemory = generateRandom(MAXIMUM_REQUIRED_MEMORY)
+            process.requiredMemory = requiredMemory
+            val requiredNetworkBandwidth = generateRandom(MAXIMUM_REQUIRED_NETWORK_BANDWIDTH)
+            process.requiredNetworkBandwidth = requiredNetworkBandwidth
+            logger.trace("Created CloudProcess with requiredCpuPower ({}), requiredMemory ({})," + " requiredNetworkBandwidth ({}).",
+                    requiredCpuPower, requiredMemory, requiredNetworkBandwidth)
+            // Notice that we leave the PlanningVariable properties on null
+            processList.add(process)
+        }
+        cloudBalance.processList = processList
+    }
+
+    private fun generateRandom(maximumValue: Int): Int {
+        val randomDouble = random.nextDouble()
+        val parabolaBase = 2000.0
+        val parabolaRandomDouble = (Math.pow(parabolaBase, randomDouble) - 1.0) / (parabolaBase - 1.0)
+        if (parabolaRandomDouble < 0.0 || parabolaRandomDouble >= 1.0) {
+            throw IllegalArgumentException("Invalid generated parabolaRandomDouble ($parabolaRandomDouble)")
+        }
+        val value = Math.floor(parabolaRandomDouble * maximumValue.toDouble()).toInt() + 1
+        if (value < 1 || value > maximumValue) {
+            throw IllegalArgumentException("Invalid generated value ($value)")
+        }
+        return value
+    }
+
+    private fun assureComputerCapacityTotalAtLeastProcessRequiredTotal(cloudBalance: CloudBalance) {
+        val computerList = cloudBalance.computerList
+        var cpuPowerTotal = 0
+        var memoryTotal = 0
+        var networkBandwidthTotal = 0
+        for (computer in computerList!!) {
+            cpuPowerTotal += computer.cpuPower
+            memoryTotal += computer.memory
+            networkBandwidthTotal += comput
